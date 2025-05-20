0669bfc6fe435d1a7ce304d688695da5e3ec30d5
AnkurkrSrivastava <ankur821305@gmail.com>
2025-05-20 14:40:00 +0530

    New PS

--- ch7/ch7_PS9.py
+++ ch7/ch7_PS9.py
@@ -1,1 +1,8 @@
-num = int(input("Enter a number to get hollow square pattern:"))
+# Floyd's Triangle
+
+p = 0
+for a in range(1, 6):
+    for b in range(1, a+1):
+        p = p + 1
+        print(p, end=" ")
+    print()    
---
+++ ch7/ch7_pattern.py
@@ -0,0 +1,23 @@
+'''for a in range (7):
+    for i in range(1,a+2):
+        print(i, end=" ")
+        i -=1    
+    print()    
+'''
+# in python
+for i in range(8):
+    for j in range(1, 8-i):
+        print(j, end=" ")
+    print()    
+
+
+'''
+in java 
+for (int i = 7; i >=1 ; i=i-2) {
+    for (int j = 1; j <= i; j++) {
+        System.out.print(j + " ");
+    }
+    System.out.println();
+}
+
+'''    
