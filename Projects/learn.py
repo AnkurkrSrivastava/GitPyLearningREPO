dcb4e1ec4d0e502e99eb29388ff44ead1fe4599d
AnkurkrSrivastava <ankur821305@gmail.com>
2025-06-07 16:34:09 +0530

    Nested to flatten

--- Projects/learn.py
+++ Projects/learn.py
@@ -1,4 +1,11 @@
-data = ("a", "b", "c")
-modify = list(data)
+# Flatten a nested tuple
+# Example: (1, (2, 3), 4) -> [1, 2, 3, 4]
 
-print(modify)
+nested = (1, (2, 3), 4)
+flat = []
+for item in nested:
+    if isinstance(item, tuple):
+        flat.extend(item)
+    else:
+        flat.append(item)
+print(flat)
