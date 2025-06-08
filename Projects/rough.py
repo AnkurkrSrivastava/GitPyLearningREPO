f9af5b8e57402115d72c45513eaa497f756c09f4
AnkurkrSrivastava <ankur821305@gmail.com>
2025-06-08 16:37:04 +0530

    Prsctice

---
+++ Projects/rough.py
@@ -0,0 +1,10 @@
+#fruits = ["apple", "banana", "cherry"]
+#first_letter = {fruit[0] for fruit in fruits}
+#print("The first letters of the fruits are:", first_letter)
+
+'''
+words = "hello world"
+my_set = {word for word in words if word != ' '}
+print(my_set)
+'''
+
---
+++ Projects/vowels.py
@@ -0,0 +1,4 @@
+word = "education"
+vowels = {char for char in word if char in "aeiou"}
+print("The word contains the following vowels:", vowels) if vowels else print("The word does not contain any vowels.")
+# This code creates a set of vowels found in the word "education" and prints them.
